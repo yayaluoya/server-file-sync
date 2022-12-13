3754fe129c80254207074eba57d0c881a78fbf61
yayaluoya <yayaluoya@sina.com>
2022-12-13 14:23:02 +0800

    删除dist包

--- dist/syncDF.d.ts
+++
@@ -1,9 +0,0 @@
-import { type Matcher } from "anymatch";
-import { SFTPWrapper } from "ssh2";
-/**
- * 同步目录和文件
- * @param localDir 本地目录
- * @param remoteDir 远程目录
- */
-export declare function syncDF(localDir: string, remoteDir: string, sftp: SFTPWrapper, ignored?: Matcher): Promise<void>;
-//# sourceMappingURL=syncDF.d.ts.map
--- dist/utils/getAbsolute.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * 获取绝对地址
- * @param _path
- * @returns
- */
-export declare function getAbsolute(_path: string): string;
-//# sourceMappingURL=getAbsolute.d.ts.map
